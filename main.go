d72638f34104a9515dbdbd259443e33e4c65b627
Anatolii Chakkaev <ac@ub.io>
2024-03-05 20:14:05 +0000

    Cleanup k8s stuff

--- main.go
+++ main.go
@@ -8,7 +8,6 @@ import (
 	"net/http"
 	"os"
 	"os/exec"
-	"path/filepath"
 	"strings"
 	"time"
 
@@ -18,12 +17,6 @@ import (
 	"github.com/gosuri/uiprogress"
 	"github.com/gosuri/uiprogress/util/strutil"
 	"github.com/hako/durafmt"
-	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
-	"k8s.io/client-go/kubernetes"
-	"k8s.io/client-go/rest"
-	"k8s.io/client-go/tools/clientcmd"
-	"k8s.io/client-go/tools/clientcmd/api"
-	"k8s.io/client-go/util/homedir"
 )
 
 func example() {
@@ -309,44 +302,44 @@ func (g *GitHubAdapter) Print(f string, a ...any) {
 	_, _ = fmt.Fprintf(g.writer, "Workflow '%s' run for %s/%s: "+f+"\n", append([]any{workflow, g.owner, g.repo}, a...)...)
 }
 
-func k8s() {
-	kubeconfig := filepath.Join(homedir.HomeDir(), ".kube", "config")
-	config, err := clientcmd.BuildConfigFromFlags("", kubeconfig)
-	if err != nil {
-		panic(err.Error())
-	}
-
-	config.AuthProvider = nil
-	config.ExecProvider = &api.ExecConfig{
-		Command:            "gke-gcloud-auth-plugin",
-		APIVersion:         "client.authentication.k8s.io/v1beta1",
-		InstallHint:        "Requires gke-gcloud-auth-plugin",
-		ProvideClusterInfo: true,
-		InteractiveMode:    api.IfAvailableExecInteractiveMode,
-	}
-
-	cl, err := rest.HTTPClientFor(config)
-	// witness.DebugClient(cl, context.TODO())
-
-	// create the clientset
-	clientset, err := kubernetes.NewForConfigAndClient(config, cl)
-	if err != nil {
-		panic(err.Error())
-	}
-
-	for {
-		pods, err := clientset.CoreV1().Pods("kube-system").List(context.TODO(), metav1.ListOptions{})
-		if err != nil {
-			panic(err.Error())
-		}
-		fmt.Printf("There are %d pods in the cluster\n", len(pods.Items))
-
-		// Examples for error handling:
-		// - Use helper functions like e.g. errors.IsNotFound()
-		// - And/or cast to StatusError and use its properties like e.g. ErrStatus.Message
-		namespace := "kube-system"
-		pods, err = clientset.CoreV1().Pods(namespace).List(context.TODO(), metav1.ListOptions{LabelSelector: "k8s-app=kube-dns"})
-
-		time.Sleep(1 * time.Second)
-	}
-}
+// func k8s() {
+// 	kubeconfig := filepath.Join(homedir.HomeDir(), ".kube", "config")
+// 	config, err := clientcmd.BuildConfigFromFlags("", kubeconfig)
+// 	if err != nil {
+// 		panic(err.Error())
+// 	}
+//
+// 	config.AuthProvider = nil
+// 	config.ExecProvider = &api.ExecConfig{
+// 		Command:            "gke-gcloud-auth-plugin",
+// 		APIVersion:         "client.authentication.k8s.io/v1beta1",
+// 		InstallHint:        "Requires gke-gcloud-auth-plugin",
+// 		ProvideClusterInfo: true,
+// 		InteractiveMode:    api.IfAvailableExecInteractiveMode,
+// 	}
+//
+// 	cl, err := rest.HTTPClientFor(config)
+// 	// witness.DebugClient(cl, context.TODO())
+//
+// 	// create the clientset
+// 	clientset, err := kubernetes.NewForConfigAndClient(config, cl)
+// 	if err != nil {
+// 		panic(err.Error())
+// 	}
+//
+// 	for {
+// 		pods, err := clientset.CoreV1().Pods("kube-system").List(context.TODO(), metav1.ListOptions{})
+// 		if err != nil {
+// 			panic(err.Error())
+// 		}
+// 		fmt.Printf("There are %d pods in the cluster\n", len(pods.Items))
+//
+// 		// Examples for error handling:
+// 		// - Use helper functions like e.g. errors.IsNotFound()
+// 		// - And/or cast to StatusError and use its properties like e.g. ErrStatus.Message
+// 		namespace := "kube-system"
+// 		pods, err = clientset.CoreV1().Pods(namespace).List(context.TODO(), metav1.ListOptions{LabelSelector: "k8s-app=kube-dns"})
+//
+// 		time.Sleep(1 * time.Second)
+// 	}
+// }
